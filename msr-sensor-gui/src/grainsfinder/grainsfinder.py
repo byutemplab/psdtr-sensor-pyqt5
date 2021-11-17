287b3b82f4323025c70e35380de1b69882059c07
gomezpaz <santigomezpaz@gmail.com>
2021-11-16 23:14:58 -0700

    Add pipeline images and resize upload button

--- msr-sensor-gui/src/grainsfinder/grainsfinder.py
+++ msr-sensor-gui/src/grainsfinder/grainsfinder.py
@@ -26,15 +26,23 @@ class UploadButton(QPushButton):
         m = e.mimeData()
         if m.hasUrls():
             image = m.urls()[0].toLocalFile()
-            map = GrainBoundariesMap(image)
-            self.parent().label_raw.setPixmap(QPixmap(map.image))
-            self.parent().label_preprocessed.setPixmap(QPixmap(map.GetContours()))
-            self.parent().label_preprocessed_2.setPixmap(QPixmap(map.DetectEdges()))
+            self.parent().map.LoadImage(image)
+            self.parent().label_raw.setPixmap(QPixmap(self.parent().map.image))
+            self.parent().label_preprocessed.setPixmap(
+                QPixmap(self.parent().map.edges_path))
+            # self.parent().label_preprocessed.setPixmap(
+            # QPixmap(self.parent().map.thresholded_path))
+            self.parent().label_preprocessed_2.setPixmap(
+                QPixmap(self.parent().map.contours_path))
+            self.parent().label_preprocessed_3.setPixmap(
+                QPixmap(self.parent().map.big_contours_path))
 
 
 class GrainsFinderTab(QWidget):
     def __init__(self):
         super().__init__()
+        # Init map object
+        self.map = GrainBoundariesMap()
         self.InitUI()
 
     def InitUI(self):
@@ -46,12 +54,37 @@ class GrainsFinderTab(QWidget):
         self.header.setText("Grains Finder")
         self.header.move(30, 20)
 
+        # Set font for all small headers
+        font = QFont()
+        font.setPointSize(8)
+
+        # Number of measurements label
+        self.header = QLabel(self)
+        self.header.setFont(font)
+        self.header.setText("Upload image")
+        self.header.move(30, 70)
+
         # Upload an image widget and button
         button = UploadButton("", self)
-        button.resize(100, 100)
+        button.resize(100, 50)
         button.setIcon(QIcon("icons/upload.png"))
-        button.setIconSize(QSize(100, 100))
-        button.move(30, 70)
+        button.setIconSize(QSize(50, 100))
+        button.move(30, 100)
+
+        # Threshold slider label
+        self.header = QLabel(self)
+        self.header.setFont(font)
+        self.header.setText("Threshold")
+        self.header.move(30 + 133, 70)
+
+        # Canny edges threshold slider
+        self.slider = QSlider(Qt.Horizontal, self)
+        self.slider.setGeometry(50, 600, 100, 50)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(1000)
+        self.slider.setValue(100)
+        self.slider.valueChanged.connect(self.UpdateThreshold)
+        self.slider.move(30 + 133, 100)
 
         self.label_raw = QLabel(self)
         self.label_raw.move(30, 200)
@@ -64,3 +97,18 @@ class GrainsFinderTab(QWidget):
         self.label_preprocessed_2 = QLabel(self)
         self.label_preprocessed_2.move(30, 420)
         self.label_preprocessed_2.resize(200, 200)
+
+        self.label_preprocessed_3 = QLabel(self)
+        self.label_preprocessed_3.move(250, 420)
+        self.label_preprocessed_3.resize(200, 200)
+
+    def UpdateThreshold(self):
+        if(self.map is not None):
+            self.map.UpdateThreshold(self.slider.value())
+            self.label_preprocessed.setPixmap(QPixmap(self.map.edges_path))
+            # self.label_preprocessed.setPixmap(
+            # QPixmap(self.map.thresholded_path))
+            self.label_preprocessed_2.setPixmap(
+                QPixmap(self.map.contours_path))
+            self.label_preprocessed_3.setPixmap(
+                QPixmap(self.map.big_contours_path))
--- msr-sensor-gui/src/grainsfinder/pipeline.py
+++ msr-sensor-gui/src/grainsfinder/pipeline.py
@@ -4,38 +4,73 @@ from PIL import Image
 
 
 class GrainBoundariesMap():
-    def __init__(self, image):
+    def __init__(self):
+        self.threshold = 100
+        self.edges_path = ''
+        self.contours_path = ''
+        self.thresholded_path = ''
+
+    def LoadImage(self, image):
         self.image = image
         self.cv2image = cv2.imread(self.image)
         self.imgrey = cv2.cvtColor(self.cv2image, cv2.COLOR_BGR2GRAY)
+        self.PreProcessing()
 
-    def GetContours(self):
+    def PreProcessing(self):
+
+        # Find edges
+        self.edges = cv2.Canny(
+            self.imgrey, self.threshold, 200, L2gradient=True)
+
+        # Save edges image
+        self.edges_path = 'grainsfinder/logs/edges.png'
+        self.Save(self.edges, self.edges_path)
+
         # Get the contours of the grains
-        ret, thresh = cv2.threshold(
-            self.imgrey, 100, 255, cv2.THRESH_BINARY_INV)
         contours, hierarchy = cv2.findContours(
-            thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            self.edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
         # Draw the contours on the image
-        self.imcontours = cv2.drawContours(
-            self.cv2image, contours, -1, (0, 255, 0), 3)
+        self.contours = self.cv2image.copy()
+        cv2.drawContours(self.contours, contours, -1, (0, 255, 0), 3)
 
-        # Save image
-        path = 'grainsfinder/logs/contours.png'
-        self.Save(self.imcontours, path)
+        # Save contours image
+        self.contours_path = 'grainsfinder/logs/contours.png'
+        self.Save(self.contours, self.contours_path)
 
-        return path
+        # Get area of each contour
+        areas = [cv2.contourArea(c) for c in contours]
+        print(areas)
+
+        # Filter out small contours
+        big_contours = [c for c in contours if cv2.contourArea(c) > 100]
+
+        # Draw the contours on the image
+        self.big_contours = self.cv2image.copy()
+        cv2.drawContours(self.big_contours, big_contours, -1, (0, 255, 0), 3)
+
+        # Save big contours image
+        self.big_contours_path = 'grainsfinder/logs/big-contours.png'
+        self.Save(self.big_contours, self.big_contours_path)
 
     def DetectEdges(self):
         # Detect edges
         edges = cv2.Canny(self.imgrey, 100, 200, L2gradient=True)
 
-        # Save image
-        path = 'grainsfinder/logs/edges.png'
-        self.Save(edges, path)
+        # Print list of edges
+        print("Edges:")
+        print(edges)
 
-        return path
+        # Save image
+        self.imedges_path = 'grainsfinder/logs/edges.png'
+        self.Save(edges, self.imedges_path)
+
+        return self.imedges_path
 
     def Save(self, file, path):
         im = Image.fromarray(file)
         im.save(path)
+
+    def UpdateThreshold(self, threshold):
+        self.threshold = threshold
+        self.PreProcessing()
